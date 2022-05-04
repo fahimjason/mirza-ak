685685d68abb9429549e996b7b14d380854ac495
JisanFahim <fahimjason@users.noreply.github.com>
2022-05-04 23:33:37 +0600

    done customization

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Apr 18, 2022',
-  name: 'Nuzhat'
+  birthdate: 'May 05, 2022',
+  name: 'Akhi'
 };
 
 function hideEverything() {
